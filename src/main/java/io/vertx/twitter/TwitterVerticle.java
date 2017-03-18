08fd6911e1c5d3ab3d18dca16a9779bfa49bf4fe
Rb Krishna <krishnarb3@gmail.com>
2017-03-18 15:56:07 +0530

    Adds cards and updating frontend page

--- src/main/java/io/vertx/twitter/TwitterVerticle.java
+++ src/main/java/io/vertx/twitter/TwitterVerticle.java
@@ -1,23 +1,19 @@
 package io.vertx.twitter;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.ext.web.handler.StaticHandler;
-import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
-import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import io.vertx.rxjava.core.RxHelper;
+import io.vertx.rxjava.ext.web.Router;
+import io.vertx.rxjava.ext.web.handler.StaticHandler;
+import io.vertx.rxjava.ext.web.handler.sockjs.SockJSHandler;
+import rx.Observable;
 import twitter4j.*;
 
-public class TwitterVerticle extends AbstractVerticle {
+public class TwitterVerticle extends io.vertx.rxjava.core.AbstractVerticle {
 
-    private String searchQuery;
-
+    //TODO: Get searchQuery from user
+    private static final String searchQuery = "vertx";
 
     public static void main(String[] args) {
 
@@ -40,71 +36,63 @@ public class TwitterVerticle extends AbstractVerticle {
         vertx.createHttpServer()
                 .requestHandler(router::accept)
                 .listen(9090, res -> {
-                   if(res.succeeded()) {
-                       updateSearch();
-                       startFuture.complete();
-                   }
-                   else startFuture.fail("Error occurred");
+                    if (res.succeeded()) {
+                        updateSearch();
+                        startFuture.complete();
+                    } else startFuture.fail("Error occurred");
                 });
     }
 
-    private SockJSHandler eventBusHandler() {
-        BridgeOptions options = new BridgeOptions();
-        return SockJSHandler.create(vertx).bridge(options, event -> {
-            if (event.type() == BridgeEventType.SOCKET_CREATED) {
-                System.out.println("Socket has been created");
-            }
-            event.complete(true);
-        });
-    }
-
-    public void defaultHandler(RoutingContext routingContext) {
-        System.out.println("default");
-    }
-
-    public void getSearchForQuery(RoutingContext routingContext) {
-        searchQuery = routingContext.request().getParam("query");
-        System.out.println(searchQuery);
-        vertx.<QueryResult>executeBlocking(future -> {
-            QueryResult result = null;
-            try {
-                Twitter twitter = TwitterFactory.getSingleton();
-                Query query = new Query("#"+ searchQuery);
-                query.setCount(100);
-                result = twitter.search(query);
-
-            } catch (TwitterException e) {
-                e.printStackTrace();
-            }
-            future.complete(result);
-        }, result -> {
-            HttpServerResponse response = routingContext.response();
-            response.setStatusCode(200).putHeader("Content-Type", "text/html; utf-8");
-            Buffer buffer = Buffer.buffer();
-            if(result.succeeded()) {
-                for (Status status : result.result().getTweets()) {
-                    buffer.appendString(status.getText()+"<br>");
-                }
-            }
-            response.putHeader("Content-Length", buffer.getBytes().length+"");
-            response.write(buffer);
-            response.end();
-        });
-    }
+//    public void getSearchForQuery(RoutingContext routingContext) {
+//        searchQuery = routingContext.request().getParam("query");
+//        System.out.println(searchQuery);
+//        vertx.<QueryResult>executeBlocking(future -> {
+//            QueryResult result = null;
+//            try {
+//                Twitter twitter = TwitterFactory.getSingleton();
+//                Query query = new Query("#"+ searchQuery);
+//                query.setCount(100);
+//                result = twitter.search(query);
+//
+//            } catch (TwitterException e) {
+//                e.printStackTrace();
+//            }
+//            future.complete(result);
+//        }, result -> {
+//            HttpServerResponse response = routingContext.response();
+//            response.setStatusCode(200).putHeader("Content-Type", "text/html; utf-8");
+//            Buffer buffer = Buffer.buffer();
+//            if(result.succeeded()) {
+//                for (Status status : result.result().getTweets()) {
+//                    buffer.appendString(status.getText()+"<br>");
+//                }
+//            }
+//            response.putHeader("Content-Length", buffer.getBytes().length+"");
+//            response.write(buffer);
+//            response.end();
+//        });
+//    }
 
     public void updateSearch() {
-        vertx.setPeriodic(8000, h -> {
-            try {
-                Thread.sleep(10000);
-                Twitter twitter = TwitterFactory.getSingleton();
-                Query query = new Query("#"+ searchQuery);
-                query.setCount(100);
-                QueryResult result = twitter.search(query);
-                vertx.eventBus().publish("publish.to.client", result);
-            } catch (InterruptedException | TwitterException e) {
-                System.out.println(e.getMessage() + " error occurred");
-            }
-        });
+
+        vertx.periodicStream(4000).toObservable()
+            .observeOn(RxHelper.schedulerHook(vertx).getNewThreadScheduler())
+            .map(v -> {
+                QueryResult result = null;
+                try {
+                    Twitter twitter = TwitterFactory.getSingleton();
+                    Query query = new Query("#" + searchQuery);
+                    query.setCount(100);
+                    result = twitter.search(query);
+                } catch (TwitterException e) {
+                    Observable.error(e);
+                }
+                return result;
+            })
+            .subscribeOn(RxHelper.scheduler(vertx))
+            .subscribe(v -> vertx.eventBus().publish("publish.to.client", "Hello")
+                    , e -> System.out.println("Error occurred")
+            );
     }
 
     private StaticHandler staticHandler() {
--- webroot/js/realtime-tweets.js
+++ webroot/js/realtime-tweets.js
@@ -7,7 +7,19 @@ function loadTweets() {
     eventBus.onopen = function () {
         console.log("Inside onopen");
         eventBus.registerHandler("publish.to.client", function (error, message) {
-            console.log("Inside register Handler");
+            console.log("Message received: " + JSON.stringify(message));
+            document.getElementById('container').innerHTML += '<div class="col s12 m4"> \
+                <div class="card blue-grey darken-1"> \
+                <div class="card-content white-text"> \
+                <span class="card-title">Card Title</span> \
+            <p>' + message.body + '</p> \
+            </div> \
+            <div class="card-action"> \
+                <a href="#">This is a link</a> \
+            <a href="#">This is a link</a> \
+            </div> \
+            </div> \
+            </div>';
         });
     };
 };
